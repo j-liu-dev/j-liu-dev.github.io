652da7002f9f463ed18240f37691ac66adf5812c
PCoder <liu.zixing@liujs.com>
2019-07-22 10:16:46 +0800

    safari < 10 support (project map)

--- js/skew.scroll.js
+++ js/skew.scroll.js
@@ -1,6 +1,9 @@
+"use strict";
+
 /*********************
  *  Helpers Code
  ********************/
+
 /**
  *  @function   DOMReady
  *
@@ -10,116 +13,103 @@
  *  @returns {*}
  *  @constructor
  */
-const DOMReady = ((
-  callback  = () => {},
-  element   = document,
-  listener  = 'addEventListener'
-) => {
-  return (element[listener]) ? element[listener]('DOMContentLoaded', callback) : window.attachEvent('onload', callback);
-});
-
+var DOMReady = function DOMReady() {
+  var callback = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : function () {};
+  var element = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : document;
+  var listener = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 'addEventListener';
+  return element[listener] ? element[listener]('DOMContentLoaded', callback) : window.attachEvent('onload', callback);
+};
 /**
  *  @function   ProjectAPI
  *
  *  @type {{hasClass, addClass, removeClass}}
  */
-const ProjectAPI = (() => {
-  let hasClass,
-      addClass,
-      removeClass;
 
-  hasClass = ((el, className) => {
+
+var ProjectAPI = function () {
+  var hasClass, addClass, removeClass;
+
+  hasClass = function hasClass(el, className) {
     if (el === null) {
       return;
     }
 
     if (el.classList) {
       return el.classList.contains(className);
-    }
-    else {
+    } else {
       return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
     }
-  });
+  };
 
-  addClass = ((el, className) => {
+  addClass = function addClass(el, className) {
     if (el === null) {
       return;
     }
 
     if (el.classList) {
       el.classList.add(className);
+    } else if (!hasClass(el, className)) {
+      el.className += ' ' + className;
     }
-    else if (!hasClass(el, className)) {
-      el.className += ' ' + className
-    }
-  });
+  };
 
-  removeClass = ((el, className) => {
+  removeClass = function removeClass(el, className) {
     if (el === null) {
       return;
     }
 
     if (el.classList) {
       el.classList.remove(className);
-    }
-    else if (hasClass(el, className)) {
-      let reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
-
+    } else if (hasClass(el, className)) {
+      var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
       el.className = el.className.replace(reg, ' ');
     }
-  });
+  };
 
   return {
-    hasClass:     hasClass,
-    addClass:     addClass,
-    removeClass:  removeClass
+    hasClass: hasClass,
+    addClass: addClass,
+    removeClass: removeClass
   };
-})();
-
-
+}();
 /*********************
  *  Application Code
  ********************/
+
 /**
  *  @function   readyFunction
  *
  *  @type {Function}
  */
-const readyFunction = (() => {
 
-  const KEY_UP    = 38;
-  const KEY_DOWN  = 40;
 
-  let scrollingClass          = 'js-scrolling',
-      scrollingActiveClass    = scrollingClass + '--active',
-      scrollingInactiveClass  = scrollingClass + '--inactive',
+var readyFunction = function readyFunction() {
+  var KEY_UP = 38;
+  var KEY_DOWN = 40;
 
-      scrollingTime           = 1350,
-      scrollingIsActive       = false,
-
-      currentPage             = 1,
-      countOfPages            = document.querySelectorAll('.' + scrollingClass + '__page').length,
-
-      prefixPage              = '.' + scrollingClass + '__page-',
-
+  var scrollingClass = 'js-scrolling',
+      scrollingActiveClass = scrollingClass + '--active',
+      scrollingInactiveClass = scrollingClass + '--inactive',
+      scrollingTime = 1350,
+      scrollingIsActive = false,
+      currentPage = 1,
+      countOfPages = document.querySelectorAll('.' + scrollingClass + '__page').length,
+      prefixPage = '.' + scrollingClass + '__page-',
       _switchPages,
       _scrollingUp,
       _scrollingDown,
-
       _mouseWheelEvent,
       _keyDownEvent,
-
       init;
-
   /**
    *  @function _switchPages
    *
    *  @private
    */
-  _switchPages = () => {
 
-    let _getPageDomEl;
 
+  _switchPages = function _switchPages() {
+    var _getPageDomEl;
     /**
      *  @function _getPageDomEl
      *
@@ -127,46 +117,30 @@ const readyFunction = (() => {
      *  @returns {Element}
      *  @private
      */
-    _getPageDomEl      = (page = currentPage) => {
+
+
+    _getPageDomEl = function _getPageDomEl() {
+      var page = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : currentPage;
       return document.querySelector(prefixPage + page);
     };
 
-    scrollingIsActive  = true;
-
-
-    ProjectAPI.removeClass(
-      _getPageDomEl(),
-      scrollingInactiveClass
-    );
-    ProjectAPI.addClass(
-      _getPageDomEl(),
-      scrollingActiveClass
-    );
-
-    ProjectAPI.addClass(
-      _getPageDomEl(currentPage - 1),
-      scrollingInactiveClass
-    );
-
-    ProjectAPI.removeClass(
-      _getPageDomEl(currentPage + 1),
-      scrollingActiveClass
-    );
-
-
-    setTimeout(
-      () => {
-        return scrollingIsActive = false;
-      },
-      scrollingTime
-    );
+    scrollingIsActive = true;
+    ProjectAPI.removeClass(_getPageDomEl(), scrollingInactiveClass);
+    ProjectAPI.addClass(_getPageDomEl(), scrollingActiveClass);
+    ProjectAPI.addClass(_getPageDomEl(currentPage - 1), scrollingInactiveClass);
+    ProjectAPI.removeClass(_getPageDomEl(currentPage + 1), scrollingActiveClass);
+    setTimeout(function () {
+      return scrollingIsActive = false;
+    }, scrollingTime);
   };
   /**
    *  @function _scrollingUp
    *
    *  @private
    */
-  _scrollingUp = () => {
+
+
+  _scrollingUp = function _scrollingUp() {
     if (currentPage === 1) {
       return false;
     }
@@ -174,6 +148,7 @@ const readyFunction = (() => {
     currentPage--;
 
     _switchPages();
+
     return true;
   };
   /**
@@ -181,7 +156,9 @@ const readyFunction = (() => {
    *
    *  @private
    */
-  _scrollingDown = () => {
+
+
+  _scrollingDown = function _scrollingDown() {
     if (currentPage === countOfPages) {
       return false;
     }
@@ -189,6 +166,7 @@ const readyFunction = (() => {
     currentPage++;
 
     _switchPages();
+
     return true;
   };
   /**
@@ -197,20 +175,22 @@ const readyFunction = (() => {
    *  @param e
    *  @private
    */
-  _mouseWheelEvent = (e) => {
+
+
+  _mouseWheelEvent = function _mouseWheelEvent(e) {
     if (scrollingIsActive) {
       e.preventDefault();
       return;
     }
 
     var scrolled = true;
+
     if (e.wheelDelta > 0 || e.detail < 0) {
       scrolled = _scrollingUp();
-    }
-    else if (e.wheelDelta < 0 || e.detail > 0) {
+    } else if (e.wheelDelta < 0 || e.detail > 0) {
       scrolled = _scrollingDown();
     }
-    
+
     if (scrolled) {
       e.preventDefault();
     }
@@ -221,47 +201,39 @@ const readyFunction = (() => {
    *  @param e
    *  @private
    */
-  _keyDownEvent = (e) => {
+
+
+  _keyDownEvent = function _keyDownEvent(e) {
     if (scrollingIsActive) {
       return;
     }
 
-    let keyCode = e.keyCode || e.which;
+    var keyCode = e.keyCode || e.which;
 
     if (keyCode === KEY_UP) {
       _scrollingUp();
-    }
-    else if (keyCode === KEY_DOWN) {
+    } else if (keyCode === KEY_DOWN) {
       _scrollingDown();
     }
-    
+
     e.preventDefault();
   };
-
   /**
    *  @function init
    *
    *  @note     auto-launch
    */
-  init = (() => {
+
+
+  init = function () {
     var scrollPage = document.querySelector('.slider-pages');
-
-    scrollPage.addEventListener(
-      'mousewheel',
-      _mouseWheelEvent,
-      false
-    );
-    scrollPage.addEventListener(
-      'DOMMouseScroll',
-      _mouseWheelEvent,
-      false
-    );
-  })();
-
-});
-
-
+    scrollPage.addEventListener('mousewheel', _mouseWheelEvent, false);
+    scrollPage.addEventListener('DOMMouseScroll', _mouseWheelEvent, false);
+  }();
+};
 /**
  *  Launcher
  */
+
+
 DOMReady(readyFunction);
